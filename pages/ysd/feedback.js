1688e75c9f5d6fdfb1eb92a3b87df6916ead5615
Yufi <yfhj1990@hotmail.com>
2016-06-25 22:51:44 +0800

    finish feedback

---
+++ pages/ysd/feedback.js
@@ -0,0 +1,41 @@
+const AreaDefaultOptions = {
+    el: null,
+    col: 5
+}
+
+class Area {
+    constructor(opts) {
+        let { el, col } = Object.assign(AreaDefaultOptions, opts)
+        this.$el = el
+        this.col = col
+        this.init()
+    }
+    init() {
+        this.setVal(this.col)
+        this.bindEvent()
+        this.changed()
+    }
+    setVal(col) {
+        this.$el.css('min-height', `${col * 14 * 1.5}px`)
+    }
+    bindEvent() {
+        this.$el.on('keyup', evt => {
+            this.$el.trigger('changed', [evt.target.value])
+        })
+    }
+    changed() {
+        this.$el.on('changed', (_, val) => {
+            let len = val.split(/\n/).length
+            this.setVal(Math.max(this.col, len + 2))
+        })
+    }
+}
+
+let $content = $('[name=content]')
+
+new Area({ el: $content })
+
+$('.js-ok').on('click', _ => {
+    let val = $content.val()
+    alert(val || '请输入评价内容')
+})
--- public/docs/readme.md
+++ public/docs/readme.md
@@ -8,11 +8,10 @@
 | password | 修改密码 |
 | address | 配送地址 |
 | address-new | 新增地址 |
-| feedback | 意见反馈 |
+
 | setting | 设置 |
 | goods | 标准产品 |
 | shoppingcar | 购物车 |
-
 | box-list | 一死得定制箱 | 1 |
 | box | 标准箱 | 1 |
 | register | 注册 |
@@ -27,3 +26,5 @@
 |-----------------------|
 | comments | 评价列表 | 2
 | comments-add | 评价 |
+|-----------------------|
+| feedback | 意见反馈 | 3 |
